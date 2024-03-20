ba2202b5e97ade42f41e95de49d7aab83c370d14
Adan <adangonamar@gmail.com>
2024-03-20 12:13:49 -0600

    Eliminando lineas duplicadas / multiplicador.py

--- primer proyecto/multiplicador.py
+++ primer proyecto/multiplicador.py
@@ -1,7 +1,4 @@
 def multiplicar():
-
-        num1 = float(input("Ingrese el primer número: "))
-        num2 = float(input("Ingrese el segundo número: "))
         resultado = num1 * num2
         return resultado
 
