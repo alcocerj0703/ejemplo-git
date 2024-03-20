0ee7760bb4bac31175be7cf10f3af48daf6a5c90
isramartin <eliu.chimal@dacodes.com.mx>
2024-03-20 10:11:14 -0600

    divicion

--- primer proyecto/division.py
+++ primer proyecto/division.py
@@ -3,3 +3,22 @@ def dividir(a, b):
         return a / b
     else:
         return "No se puede dividir por cero"
+
+# from validador import validar
+
+# def dividir(a, b):
+#     es_valido, mensaje = validar(b)
+#     if not es_valido:
+#         return mensaje
+#     else:
+#         if b != 0:
+#             return a / b
+#         else:
+#             return "No se puede dividir por cero"
+
+# # Ejemplo de uso:
+# resultado = dividir(10, 0)
+# print(resultado)  # Salida: "No se puede dividir por cero"
+
+# resultado = dividir(10, 5)
+# print(resultado)  # Salida: 2.0
