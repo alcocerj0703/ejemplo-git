4053bba938b64a896987cd891a4e848b8e935127
isai <isaix_@hotmail.com>
2024-03-20 12:25:07 -0600

    compuesto

---
+++ primer proyecto/multiplicacion.py
@@ -0,0 +1,4 @@
+def multiplicar(num1,num2):
+        resultado = num1 * num2
+        return resultado
+
--- primer proyecto/multiplicador.py
+++
@@ -1,7 +0,0 @@
-def multiplicar():
-        resultado = num1 * num2
-        return resultado
-
-# Llamada a la función
-resultado = multiplicar()
-print("El resultado de la multiplicación es:", resultado)
